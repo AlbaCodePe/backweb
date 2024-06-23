84c2db32b8859fb80d2622c124df2c4d27280055
Brando Daniel Romero Loma <u202015623@upc.edu.pe>
2024-06-23 18:57:36 -0500

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+// Register
+router.post('/register', async (req, res) => {
+  const { dni, fullName, birthDate, email, username, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    user = new User({
+      dni,
+      fullName,
+      birthDate,
+      email,
+      username,
+      password
+    });
+
+    await user.save();
+
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: '5h' },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
